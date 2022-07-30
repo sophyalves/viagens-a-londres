06779e2ec513c6da48df4ba8ca91de05e235c020
sophyalves <109874929+sophyalves@users.noreply.github.com>
2022-07-30 16:57:00 -0300

    Update script.js

---
+++ london sophia/script.js
@@ -0,0 +1,17 @@
+unction logar()   {
+  
+    var login= document.getElementById("login").value;
+    var senha=document.getElementById("senha").value;
+
+    if(login== "destinos de viagens" && semha == "londres"){
+
+       alert("login realizado com sucesso");
+       location.href="home.html";
+    } 
+        else {
+        alert("usuario ou senha incorretos")
+
+    }
+
+
+}
